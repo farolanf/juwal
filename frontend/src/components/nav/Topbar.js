8a3b22f96c1ebe2290d2e5d9b510489d503bdee0
Farolan Faisal <farolanfaisal@gmail.com>
2019-01-04 16:49:12 +0800

    add tailwind

--- frontend/src/components/nav/Topbar.js
+++ frontend/src/components/nav/Topbar.js
@@ -1,31 +1,45 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { compose } from 'lodash/fp'
 
+import { withStyles } from '@material-ui/core/styles'
 import withWidth, { isWidthUp } from '@material-ui/core/withWidth'
 
 import AppBar from '@material-ui/core/AppBar'
 import Typography from '@material-ui/core/Typography'
 import Toolbar from '@material-ui/core/Toolbar'
+import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/MenuOutlined'
+import PersonIcon from '@material-ui/icons/Person'
 
 import Spacer from '../Spacer'
 import Link from './Link'
 
-const Topbar = ({ title, width }) => (
+const styles = {
+  spaceRight: tw`mr-2`
+}
+
+const Topbar = ({ title, width, classes }) => (
   <AppBar position='static'>
     <Toolbar>
       {isWidthUp('md', width) && (
-        <IconButton color='inherit' aria-label='Menu'>
+        <IconButton color='inherit' className={classes.spaceRight} aria-label='Menu'>
           <MenuIcon />
         </IconButton>
       )}
-      <Link to='/'>
-        <Typography variant='h5' color='inherit'>{title}</Typography>
+      <Link to='/' className={classes.spaceRight}>
+        <Typography variant='h6' color='inherit'>{title}</Typography>
       </Link>
+      <Button color='inherit'>
+        <Typography variant='button' color='inherit'>Home</Typography>
+      </Button>
+      <Button color='inherit'>
+        <Typography variant='button' color='inherit'>Categories</Typography>
+      </Button>
       <Spacer />
       <IconButton color='inherit' aria-label='Account'>
-        <MenuIcon />
+        <PersonIcon />
       </IconButton>
     </Toolbar>
   </AppBar>
@@ -34,6 +48,7 @@ const Topbar = ({ title, width }) => (
 Topbar.propTypes = {
   title: PropTypes.string.isRequired,
   width: PropTypes.string.isRequired,
+  classes: PropTypes.object.isRequired,
 }
 
-export default withWidth()(Topbar)
+export default compose(withWidth(), withStyles(styles))(Topbar)
