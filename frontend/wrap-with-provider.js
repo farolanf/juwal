123005dec2adbd06049b5fae2cdcc513692ea1ee
Farolan Faisal <farolanfaisal@gmail.com>
2019-01-20 16:47:18 +0800

    setup redux devtools

--- frontend/wrap-with-provider.js
+++ frontend/wrap-with-provider.js
@@ -1,14 +1,18 @@
 const React = require('react')
 const PropTypes = require('prop-types')
 const { Provider } = require('react-redux')
-const { createStore, applyMiddleware } = require('redux')
+const { createStore, applyMiddleware, compose } = require('redux')
 const reduxThunk = require('redux-thunk').default
 
 const reducers = require('./src/reducers').default
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const store = createStore(
   reducers,
-  applyMiddleware(reduxThunk)
+  composeEnhancers(
+    applyMiddleware(reduxThunk)
+  )
 )
 
 const wrapWithProvider = ({ element }) => (
