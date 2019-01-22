0f3e04ba054df87756f2ed84656e340d28b64fd9
Farolan Faisal <farolanfaisal@gmail.com>
2019-01-22 12:23:26 +0800

    persist locale

--- frontend/gatsby-browser.js
+++ frontend/gatsby-browser.js
@@ -4,6 +4,18 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+ // init store
+ 
+const store = require('$src/store').default
+const { initAuthorization, loadToken } = require('$src/modules/auth')
+const { loadLocale } = require('$src/modules/app')
+const { fetchUser } = require('$act/account')
+const { setLocale } = require('$act/app')
+
+initAuthorization()
+loadToken() && store.dispatch(fetchUser())
+loadLocale() && store.dispatch(setLocale({ locale: loadLocale() }))
+
 const wrapWithProvider = require('./wrap-with-provider')
 
 module.exports = {
--- frontend/wrap-with-provider/redux.js
+++ frontend/wrap-with-provider/redux.js
@@ -1,12 +1,6 @@
 const React = require('react')
 const { Provider } = require('react-redux')
-
 const store = require('$src/store').default
-const { initAuthorization, loadToken } = require('$src/modules/auth')
-const { fetchUser } = require('$act/account')
-
-initAuthorization()
-loadToken() && store.dispatch(fetchUser())
 
 const wrapWithProvider = element => (
   <Provider store={store}>
