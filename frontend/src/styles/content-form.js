1f34fe9c42d26ee0960a16ed1bf86a8fafd611a7
Farolan Faisal <farolanfaisal@gmail.com>
2019-01-25 07:31:46 +0800

    update style

--- frontend/src/styles/content-form.js
+++ frontend/src/styles/content-form.js
@@ -2,7 +2,7 @@ export default theme => ({
   ...tw`pt-10 pb-20`,
   [theme.breakpoints.up('md')]: tw`pt-12 pb-24`,
   '& > *': {
-    ...tw`m-auto`,
+    ...tw`mx-auto`,
     width: 'calc(100% - 32px)',
     [theme.breakpoints.up('md')]: {
       width: theme.breakpoints.values.sm
