ad435c92144c0bc0794242b53dba2a7dd2a32ee6
Farolan Faisal <farolanfaisal@gmail.com>
2019-01-25 17:32:16 +0800

    add price

--- frontend/src/pages/pasang-iklan.js
+++ frontend/src/pages/pasang-iklan.js
@@ -6,6 +6,12 @@ import { compose } from 'lodash/fp'
 import { withStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import Button from '@material-ui/core/Button'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+
+import NumberFormat from 'react-number-format'
 
 import Layout from '$comp/layout'
 
@@ -31,6 +37,14 @@ const messages = defineMessages({
   descHelp: {
     id: 'ads.new.descHelp',
     defaultMessage: 'Describe the product in detail, with minus, if any. Be honest, people will like it.'
+  },
+  price: {
+    id: 'ads.new.price',
+    defaultMessage: 'Price'
+  },
+  nego: {
+    id: 'ads.new.nego',
+    defaultMessage: 'Nego'
   }
 })
 
@@ -39,8 +53,28 @@ const styles = theme => ({
   help: {
     ...tw`flex justify-between`,
     '& > :first-child': tw`mr-3`
-  }
+  },
+  priceRow: {
+    ...tw`flex items-end`,
+    '& > :first-child': tw`mr-4`
+  },
+  price: {
+    '& input': tw`text-right`
+  },
+  actions: tw`mt-6`
 })
+
+// eslint-disable-next-line react/prop-types
+const CurrencyFormat = ({ inputRef, onChange, ...other }) => (
+  <NumberFormat
+    {...other}
+    getInputRef={inputRef}
+    onValueChange={values => onChange(values.floatValue)}
+    thousandSeparator
+    isAllowed={values => values.floatValue <= 1e14 || values.value === ''}
+    allowNegative={false}
+  />
+)
 
 const Help = withStyles(styles)(
   ({ classes, text, length, maxLength }) => (
@@ -57,24 +91,29 @@ class PasangIklanPage extends React.Component {
     this.state = {
       title: '',
       desc: '',
+      price: '',
+      nego: false
     }
   }
 
-  onTitleChange = e => {
-    this.setState({ title: e.target.value })
-  }
+  onInputChange = name => e => this.setState({ [name]: e.target.value })
 
-  onDescChange = e => {
-    this.setState({ desc: e.target.value })
+  onCheckedChange = name => e => this.setState({ [name]: e.target.checked })
+
+  onValueChange = name => val => this.setState({ [name]: val })
+
+  onSubmit = e => {
+    e.preventDefault()
+    console.log('submit')
   }
 
   render () {
     const { classes, intl } = this.props
-    const { title, desc } = this.state
+    const { title, desc, price, nego } = this.state
     return (
       <Layout>
         <div className={classes.content}>
-          <div>
+          <form onSubmit={this.onSubmit}>
             <Typography variant='h4' gutterBottom>
               <FormattedMessage {...messages.pageTitle} />
             </Typography>
@@ -84,7 +123,7 @@ class PasangIklanPage extends React.Component {
               margin='normal'
               inputProps={{ maxLength: 70 }}
               value={title}
-              onChange={this.onTitleChange}
+              onChange={this.onInputChange('title')}
               helperText={
                 <Help
                   text={intl.formatMessage(messages.titleHelp)}
@@ -102,7 +141,7 @@ class PasangIklanPage extends React.Component {
               margin='normal'
               inputProps={{ maxLength: 4000 }}
               value={desc}
-              onChange={this.onDescChange}
+              onChange={this.onInputChange('desc')}
               helperText={
                 <Help
                   text={intl.formatMessage(messages.descHelp)}
@@ -111,7 +150,34 @@ class PasangIklanPage extends React.Component {
                 />
               }
             />
-          </div>
+            <div className={classes.priceRow}>
+              <TextField
+                label={intl.formatMessage(messages.price)}
+                margin='normal'
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position='start'>Rp</InputAdornment>
+                  ),
+                  inputComponent: CurrencyFormat
+                }}
+                value={price}
+                onChange={this.onValueChange('price')}
+                className={classes.price}
+              />
+              <FormControlLabel
+                label={intl.formatMessage(messages.nego)}
+                control={
+                  <Checkbox
+                    checked={nego}
+                    onChange={this.onCheckedChange('nego')}
+                  />
+                }
+              />
+            </div>
+            <div className={classes.actions}>
+              <Button type='submit' variant='contained' color='primary'>Submit</Button>
+            </div>
+          </form>
         </div>
       </Layout>
     )
